ed427cf3bcfe94c165c9cee1d73955183bc91bbe
kwon-evan <koun012@naver.com>
2023-01-20 11:53:30 +0900

    fix predict.py

--- predict.py
+++ predict.py
@@ -7,13 +7,12 @@ from argparse import Namespace
 import warnings
 import yaml
 import torch
-import pandas as pd
+from cv2 import cv2
 from rich import print
+from rich.progress import track
 from sklearn.metrics import accuracy_score
-from pytorch_lightning import Trainer
 
 from lprnet.lprnet import LPRNet
-from lprnet.datamodule import DataModule
 
 warnings.filterwarnings("ignore")
 
@@ -23,30 +22,14 @@ if __name__ == '__main__':
         args = Namespace(**yaml.load(f, Loader=yaml.FullLoader))
 
     load_model_start = time.time()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     lprnet = LPRNet(args)
     lprnet.load_state_dict(torch.load(args.pretrained))
-    lprnet.eval()
+    lprnet.to(device).eval()
     print(f"Successful to build network in {time.time() - load_model_start}s")
 
-    dm = DataModule(args)
+    imgs = os.listdir(args.test_dir)[:100]
+    labels = [n.split('.')[0].split('-')[0] for n in track(imgs)]
+    preds = [lprnet.detect(cv2.imread(args.test_dir + img), device) for img in track(imgs)]
 
-    trainer = Trainer(
-            accelerator="auto",
-            devices=torch.cuda.device_count(),
-    )
-
-    since = time.time()
-    preds = trainer.predict(lprnet, dm)
-    preds = sum(preds, [])
-    real = list(map(lambda x: x.split('.')[0].split('-')[0], os.listdir(args.test_dir)))
-
-    result = pd.DataFrame({'real': real, 'pred': preds})
-
-    print(result)
-
-    img_cnt = len(os.listdir(args.test_dir))
-    time_total = time.time() - since
-
-    print("model inference in {:2.3f} seconds".format(time_total))
-    print("Accuracy: ", accuracy_score(real, preds))
-    print(f"img/ms: {time_total/img_cnt * 1000}")
+    print("Accuracy: ", accuracy_score(labels, preds))
