8be12e1118c387035db9e907d728c8221c9aedd8
kwon-evan <koun012@naver.com>
2023-02-23 11:50:49 +0900

    code clean

--- scripts/TPS.py
+++ scripts/TPS.py
@@ -12,29 +12,29 @@ warnings.filterwarnings(action="ignore")
 
 
 def TPS(model, opt):
-    # IMAGE_FOLDER = '/home/fourind/projects/datas/kor-plates/test'
-    IMAGE_FOLDER = "demo_images"
-    for img_name in sorted(os.listdir(IMAGE_FOLDER)):
-        image = Image.open(f"{IMAGE_FOLDER}/{img_name}")
+    # SRC = '/home/fourind/projects/datas/kor-plates/test'
+    SRC = "demo_images"
+    DST = "warped"
+    os.makedirs(DST, exist_ok=True)
+    for img_name in sorted(os.listdir(SRC)):
+        image = Image.open(f"{SRC}/{img_name}")
         image_size = image.size
-        label = img_name.split(".")[0].split("-")[0]
 
         to_tensor = transforms.ToTensor()
         to_pil = transforms.ToPILImage()
 
         image = image.resize((opt.imgW, opt.imgH), Image.ANTIALIAS)
         image = to_tensor(image).to(device).unsqueeze(0)
-        # image.sub_(0.5).div_(0.5) # Image Normalize
+        image.sub_(0.5).div_(0.5)  # Image Normalize
 
         """ Transformation stage """
-        if not model.stages["Trans"] == "None":
-            warped = model.Transformation(image)
+        warped = model.Transformation(image)
 
+        warped.mul_(0.5).add_(0.5)  # Image Denormalize
         warped = warped.squeeze(0)
         warped = to_pil(warped)
         warped = warped.resize(image_size, Image.ANTIALIAS)
-        rename = img_name.split(".")[0] + "-warped.jpg"
-        warped.save(f"{IMAGE_FOLDER}/{rename}", dpi=(200, 200))
+        warped.save(f"{DST}/{img_name}", dpi=(200, 200))
 
 
 if __name__ == "__main__":
