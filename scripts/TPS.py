8280427d72c492e9a397e60d277fb4e47ee5c212
kwon-evan <koun012@naver.com>
2023-02-23 11:43:43 +0900

    clean scripts

--- scripts/TPS.py
+++ scripts/TPS.py
@@ -1,32 +1,23 @@
 import os
-from argparse import Namespace
-import yaml
 import warnings
 
 import torch
 import torch.utils.data
-from rich import print
 from PIL import Image
 import torchvision.transforms as transforms
 
-from lprnet import Model
+from lprnet import load_LPRNet
 
-warnings.filterwarnings(action='ignore')
+warnings.filterwarnings(action="ignore")
 
-def TPS(opt):
-    if opt.saved_model == '' or os.path.exists(opt.saved_model):
-        assert f'{opt.saved_model} is not exist!'
 
-    model = Model.load_from_checkpoint(opt.saved_model, opt=opt)
-    model.eval().to(device)
-    print(f'model loaded from checkpoint {opt.saved_model}')
-
+def TPS(model, opt):
     # IMAGE_FOLDER = '/home/fourind/projects/datas/kor-plates/test'
-    IMAGE_FOLDER = 'demo_images'
+    IMAGE_FOLDER = "demo_images"
     for img_name in sorted(os.listdir(IMAGE_FOLDER)):
-        image = Image.open(f'{IMAGE_FOLDER}/{img_name}')
+        image = Image.open(f"{IMAGE_FOLDER}/{img_name}")
         image_size = image.size
-        label = img_name.split('.')[0].split('-')[0]
+        label = img_name.split(".")[0].split("-")[0]
 
         to_tensor = transforms.ToTensor()
         to_pil = transforms.ToPILImage()
@@ -36,21 +27,20 @@ def TPS(opt):
         # image.sub_(0.5).div_(0.5) # Image Normalize
 
         """ Transformation stage """
-        if not model.stages['Trans'] == "None":
+        if not model.stages["Trans"] == "None":
             warped = model.Transformation(image)
 
         warped = warped.squeeze(0)
         warped = to_pil(warped)
         warped = warped.resize(image_size, Image.ANTIALIAS)
-        rename = img_name.split('.')[0] + '-warped.jpg'
-        warped.save(f'{IMAGE_FOLDER}/{rename}', dpi=(200, 200))
+        rename = img_name.split(".")[0] + "-warped.jpg"
+        warped.save(f"{IMAGE_FOLDER}/{rename}", dpi=(200, 200))
 
 
-if __name__ == '__main__':
-    """ load configuration """
-    with open('config-idn.yaml', 'r') as f:
-        opt = yaml.safe_load(f)
-        opt = Namespace(**opt)
+if __name__ == "__main__":
+    """load configuration"""
+    model, opt = load_LPRNet("config-idn.yaml")
+    model.eval().freeze()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    TPS(opt)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    TPS(model, opt)
--- scripts/predict_each.py
+++ scripts/predict_each.py
@@ -1,86 +1,51 @@
 import os
-import random
-import string
-from argparse import Namespace
-import yaml
 import warnings
 
 import torch
-import torch.backends.cudnn as cudnn
 import torch.utils.data
-import numpy as np
 import pandas as pd
 from rich import print
 from PIL import Image
 
-from lprnet import Model
+from lprnet import load_LPRNet
 
-warnings.filterwarnings(action='ignore')
+warnings.filterwarnings(action="ignore")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def predict(opt):
-    if opt.saved_model == '' or os.path.exists(opt.saved_model):
-        assert f'{opt.saved_model} is not exist!'
-
-    model = Model.load_from_checkpoint(opt.saved_model, opt=opt)
-    model.eval().to(device)
-    # model.freeze()
-    print(f'model loaded from checkpoint {opt.saved_model}')
-
+def predict(model, opt):
+    model.to(device)
     print(model.hparams)
 
     # IMAGE_FOLDER = '/home/fourind/projects/datas/kor-plates/test'
-    IMAGE_FOLDER = 'demo_images/'
+    IMAGE_FOLDER = "demo_images/"
     result = {
-        'img_names': [],
-        'labels': [],
-        'preds': [],
-        'confs': [],
-        'times': [],
-        'acc': []
+        "img_names": [],
+        "labels": [],
+        "preds": [],
+        "confs": [],
+        "times": [],
+        "acc": [],
     }
     for img_name in os.listdir(IMAGE_FOLDER):
-        img = Image.open(f'{IMAGE_FOLDER}/{img_name}')
-        label = img_name.split('.')[0].split('-')[0]
+        img = Image.open(f"{IMAGE_FOLDER}/{img_name}")
+        label = img_name.split(".")[0].split("-")[0]
 
         pred, conf, time = model.imread(img, device)
-        result['img_names'].append(img_name)
-        result['labels'].append(label)
-        result['preds'].append(pred)
-        result['confs'].append(conf)
-        result['times'].append(time)
-        result['acc'].append(label == pred)
+        result["img_names"].append(img_name)
+        result["labels"].append(label)
+        result["preds"].append(pred)
+        result["confs"].append(conf)
+        result["times"].append(time)
+        result["acc"].append(label == pred)
 
     df = pd.DataFrame(result)
     print(df)
 
 
-if __name__ == '__main__':
-    """ load configuration """
-    with open('config-idn.yaml', 'r') as f:
-        opt = yaml.safe_load(f)
-        opt = Namespace(**opt)
+if __name__ == "__main__":
+    """load configuration"""
+    model, opt = load_LPRNet("config-idn.yaml")
+    model.eval().freeze()
 
-    if not opt.exp_name:
-        opt.exp_name = f'{opt.Transformation}-{opt.FeatureExtraction}-{opt.SequenceModeling}-{opt.Prediction}'
-        opt.exp_name += f'-Seed{opt.manualSeed}'
-        # print(opt.exp_name)
-
-    """ vocab / character number configuration """
-    if opt.sensitive:
-        # opt.character += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
-
-    """ Seed and GPU setting """
-    random.seed(opt.manualSeed)
-    np.random.seed(opt.manualSeed)
-    torch.manual_seed(opt.manualSeed)
-    torch.cuda.manual_seed(opt.manualSeed)
-
-    cudnn.benchmark = True
-    cudnn.deterministic = True
-    opt.num_gpu = torch.cuda.device_count()
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    predict(opt)
+    predict(model, opt)
