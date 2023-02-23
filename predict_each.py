46eb9448ab15cebdf6b8f0d78ac6a462558313c7
kwon-evan <koun012@naver.com>
2023-02-23 11:44:02 +0900

    rm useless

---
+++ predict_each.py
@@ -0,0 +1,57 @@
+import os
+import random
+import string
+from argparse import Namespace
+import yaml
+import warnings
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import numpy as np
+import pandas as pd
+from rich import print
+from PIL import Image
+
+from lprnet import Model, load_LPRNet
+
+warnings.filterwarnings(action="ignore")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def predict(model, opt):
+    model.to(device)
+    print(model.hparams)
+
+    # IMAGE_FOLDER = '/home/fourind/projects/datas/kor-plates/test'
+    IMAGE_FOLDER = "demo_images/"
+    result = {
+        "img_names": [],
+        "labels": [],
+        "preds": [],
+        "confs": [],
+        "times": [],
+        "acc": [],
+    }
+    for img_name in os.listdir(IMAGE_FOLDER):
+        img = Image.open(f"{IMAGE_FOLDER}/{img_name}")
+        label = img_name.split(".")[0].split("-")[0]
+
+        pred, conf, time = model.imread(img, device)
+        result["img_names"].append(img_name)
+        result["labels"].append(label)
+        result["preds"].append(pred)
+        result["confs"].append(conf)
+        result["times"].append(time)
+        result["acc"].append(label == pred)
+
+    df = pd.DataFrame(result)
+    print(df)
+
+
+if __name__ == "__main__":
+    """load configuration"""
+    model, opt = load_LPRNet("config-idn.yaml")
+    model.eval().freeze()
+
+    predict(model, opt)
