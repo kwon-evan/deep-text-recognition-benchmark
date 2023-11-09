1ea1912bd0f76887fc3142436944b2f5ccebf560
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-11-09 13:50:28 +0900

    chore: rename package

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="LPRNet",
+    name="TRBA",
     version="0.0.3",
     author="Heonjin Kwon",
     author_email="kwon@4ind.co.kr",
     description="A Package for license plate recognition written in pytorch-lightning",
-    keywords=['pytorch', 'pytorch-lightning', 'license-plate-recognition'],
+    keywords=["pytorch", "pytorch-lightning", "license-plate-recognition"],
     install_requires=[
         # LPRNet requirements
-        'pytorch-lightning>=1.7.0, <=1.9.0',
-        'numpy>=1.17.1',
-        'Pillow==9.4.0',
-        'nltk==3.8.1',
+        "pytorch-lightning>=1.7.0, <=1.9.0",
+        "numpy>=1.17.1",
+        "Pillow==9.4.0",
+        "nltk==3.8.1",
         # 'imutils>=0.4.0',
-        'multidict',
-        'attrs',
-        'yarl',
-        'idna_ssl',
-        'charset_normalizer',
-        'aiosignal',
+        "multidict",
+        "attrs",
+        "yarl",
+        "idna_ssl",
+        "charset_normalizer",
+        "aiosignal",
     ],
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
